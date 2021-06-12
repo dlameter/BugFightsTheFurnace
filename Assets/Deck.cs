b277f87ed9b84bf0521059133e5a99a05a47929c
Dominik <dominik@lameter.com>
2021-06-12 18:07:00 -0500

    add deck

--- Assets/Deck.cs
+++ Assets/Deck.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Deck : MonoBehaviour
+public class Deck : MonoBehaviour, IBeginDragHandler, IPointerDownHandler
 {
     public static int MAX_CARDS = 10;
+    public GameObject cardPrefab;
+    public GameObject cardParent;
     public List<BugStats> bugList;
 
     public BugStats DrawCard()
     {
-        int randomIndex = (int) Random.Range(0, bugList.Count);
+        int randomIndex = Mathf.RoundToInt(Random.Range(0, bugList.Count));
         return bugList[randomIndex];
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        BuildCard(DrawCard());
+    }
+
+    private GameObject BuildCard(BugStats bugStats)
+    {
+        GameObject cardObject = Instantiate(cardPrefab, this.GetComponent<RectTransform>().anchoredPosition, Quaternion.identity, cardParent.transform) as GameObject;
+        cardObject.GetComponent<Card>().bugStats = bugStats;
+        return cardObject;
     }
 }
