56f0292390ca883e06bc0c35ea618bf79fe92a4d
Dominik <dominik@lameter.com>
2021-06-12 17:27:57 -0500

    add draw function to deck

--- Assets/Deck.cs
+++ Assets/Deck.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
     public static int MAX_CARDS = 10;
-    public List<Card> cards;
+    public List<BugStats> bugList;
+
+    public BugStats DrawCard()
+    {
+        int randomIndex = (int) Random.Range(0, bugList.Count);
+        return bugList[randomIndex];
+    }
 }
