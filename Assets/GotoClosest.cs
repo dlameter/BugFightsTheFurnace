99c9926fcee0b97b2243639ad66d93536d93c258
Dominik <dominik@lameter.com>
2021-06-11 23:36:10 -0500

    add basic bug chess AI

--- Assets/GotoClosest.cs
+++ Assets/GotoClosest.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class GotoClosest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float stopDistance;
+    public bool friend;
+
+    private HashSet<Collider2D> colliding = new HashSet<Collider2D>();
+
+    void FixedUpdate()
     {
-        
+        Transform closest = GetClosestTransform(new List<Collider2D>(colliding).ConvertAll<Transform>(Collider2DToTransform));
+        Debug.Log(closest);
+        MoveTo(closest);
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveTo(Transform transform)
     {
-        
+        Rigidbody2D body = this.GetComponent<Rigidbody2D>();
+        if (body)
+        {
+            Vector3 position = body.position;
+            Vector3 otherPosition = transform.position;
+            Vector3 difference = otherPosition - position;
+
+            body.SetRotation(Vector3.Angle(difference, Vector3.back));
+
+            if (Vector3.Distance(position, otherPosition) > stopDistance)
+            {
+                body.velocity = difference.normalized * speed * Time.deltaTime;
+            }
+            else
+            {
+                body.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    Transform Collider2DToTransform(Collider2D collider)
+    {
+        return collider.transform;
+    }
+
+    Transform GetClosestTransform(List<Transform> transforms)
+    {
+        Rigidbody2D body = this.GetComponent<Rigidbody2D>();
+        if (!body || transforms.Count == 0)
+        {
+            return null;
+        }
+        Transform closest = transforms[0];
+
+        foreach (Transform transform in transforms)
+        {
+            if (Vector3.Distance(body.transform.position, transform.position) < Vector3.Distance(body.transform.position, closest.position))
+            {
+                closest = transform;
+            }
+        }
+
+        return closest;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bug")
+        {
+            this.colliding.Add(other);
+        }
+    }
+
+    void OnTriggerLeave2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bug")
+        {
+            this.colliding.Remove(other);
+        }
     }
 }
