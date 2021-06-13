5fe05c0e82f8c65d26a326b3d457db1701c5e02b
Josh Simmons <simmonsj711@dupage.edu>
2021-06-12 19:42:09 -0500

    bug fix/maggot update

--- Assets/GotoClosest.cs
+++ Assets/GotoClosest.cs
@@ -27,7 +27,7 @@ public class GotoClosest : MonoBehaviour
     void MoveTo(Transform transform)
     {
         if(!transform || !body){
-            body.velocity = Vector2.zero;
+            if(body)body.velocity = Vector2.zero;
             return;
         }
         Vector3 position = body.position;
